a433c83a02e8d9b8c0581d55fa7d22623185a185
Chris Brinley <cbrinley219@gmail.com>
2013-07-20 23:07:40 +0000

    making cookbook update more specific

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Sets up various aliases that are helpful for working with these cookbooks interactively
 function upcb(){
-  /bin/cp -r /root/src/chef/chef_glusterfs/* /var/chef/cookbooks/.
+  /bin/cp -r /root/src/chef/chef_glusterfs/glusterfs /var/chef/cookbooks/.
+  /bin/cp -r /root/src/chef/lvm /var/chef/cookbooks/.
   echo "cookbooks updated"
 }
 
