692e3714d61c865f72344ed579849055facf23dd
Chris Brinley <cbrinley219@gmail.com>
2013-07-20 22:53:41 +0000

    adding aliases that are handy for development

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Sets up various aliases that are helpful for working with these cookbooks interactively
+function upcb(){
+  /bin/cp -r /root/src/chef/chef_glusterfs/* /var/chef/cookbooks/.
+  echo "cookbooks updated"
+}
