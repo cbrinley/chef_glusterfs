aa33aacd682b6916ebb021fb3969fdde588c81ab
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-25 14:13:33 -0500

    adding name to lvm resource

--- glusterfs/recipes/lvm.rb
+++ glusterfs/recipes/lvm.rb
@@ -1,6 +1,6 @@
 lvm_volume_group "gluster_vg000" do
   physical_volumes default.glusterfs.physical_volumes 
-  logical_volume '' do
+  logical_volume 'glusterfs' do
         size '100%VG'
         filesystem 'xfs'
         mount_point :location => default.glusterfs.brick_path, :options => 'size=512'
