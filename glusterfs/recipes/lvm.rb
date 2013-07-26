9a594986544476cfeb33922cc16b0f6080caa51d
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-25 21:14:38 -0500

    Adding directory resource

--- glusterfs/recipes/lvm.rb
+++ glusterfs/recipes/lvm.rb
@@ -2,17 +2,21 @@
 lvm_volume_group "gluster_vg000" do
   physical_volumes node.glusterfs.physical_volumes 
   logical_volume 'glusterfs' do
-        size '100%VG'
-        filesystem 'xfs'
-        mount_point :location => node.glusterfs.brick_path, :options => 'size=512'
-        action :create
+  	size '100%VG'
+	filesystem 'xfs'
+    mount_point :location => node.glusterfs.brick_path, :options => 'size=512'
+    action :create
   end
   action :create
 end
 
+directory node.glusterfs.brick_path do
+  action :create
+end
+
 mount node.glusterfs.brick_path do
-	fstype "xfs"
-	options "size=512"
-	device "/dev/mapper/gluster_vg000-glusterfs"
-	action [:mount, :enable]
+  fstype "xfs"
+  options "size=512"
+  device "/dev/mapper/gluster_vg000-glusterfs"
+  action [:mount, :enable]
 end
