1315cea8e04b61a3cbd334b1761753c06b187ee6
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-25 20:20:04 -0500

    Logging

--- glusterfs/recipes/lvm.rb
+++ glusterfs/recipes/lvm.rb
@@ -1,11 +1,1 @@
 puts default.glusterfs.physical_volumes
-=begin
-lvm_volume_group "gluster_vg000" do
-  physical_volumes default.glusterfs.physical_volumes 
-  logical_volume 'glusterfs' do
-        size '100%VG'
-        filesystem 'xfs'
-        mount_point :location => default.glusterfs.brick_path, :options => 'size=512'
-  end
-end
-=end
