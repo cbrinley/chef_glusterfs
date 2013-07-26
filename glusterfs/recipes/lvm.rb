f2a10f180681834eb3edbe8877cbc58a088498b6
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-25 20:39:20 -0500

    Updated lvm recipe

--- glusterfs/recipes/lvm.rb
+++ glusterfs/recipes/lvm.rb
@@ -1,1 +1,9 @@
-puts default.glusterfs.physical_volumes
+
+lvm_volume_group "gluster_vg000" do
+  physical_volumes node.glusterfs.physical_volumes 
+  logical_volume 'glusterfs' do
+        size '100%VG'
+        filesystem 'xfs'
+        mount_point :location => node.glusterfs.brick_path, :options => 'size=512'
+  end
+end
