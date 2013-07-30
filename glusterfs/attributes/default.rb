eea7ffa034d7910d2932b87feec0fe29a79bb541
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-30 15:23:30 -0500

    Updating attributes to support services recipe

--- glusterfs/attributes/default.rb
+++ glusterfs/attributes/default.rb
@@ -1,2 +1,5 @@
 default["glusterfs"]["physical_volumes"] = ["/dev/xvdj"]
 default["glusterfs"]["brick_path"] = "/data/gluster/brick1"
+default["glusterfs"]["services"]["all"]["enabled"] = true
+default["glusterfs"]["services"]["swift"]["enabled"] = true
+default["glusterfs"]["services"]["gluster"]["enabled"] = true
