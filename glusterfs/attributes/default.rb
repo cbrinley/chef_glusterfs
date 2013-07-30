5f8b50a5ed38684eb3df1c73457fb84f6aa8effd
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-30 15:29:36 -0500

    Adding doc to attribs file

--- glusterfs/attributes/default.rb
+++ glusterfs/attributes/default.rb
@@ -1,5 +1,7 @@
 default["glusterfs"]["physical_volumes"] = ["/dev/xvdj"]
 default["glusterfs"]["brick_path"] = "/data/gluster/brick1"
+
+#Service related flags. Turn all services off with the all flag, or swift/gluster with respective subsets
 default["glusterfs"]["services"]["all"]["enabled"] = true
 default["glusterfs"]["services"]["swift"]["enabled"] = true
 default["glusterfs"]["services"]["gluster"]["enabled"] = true
