08aae15928822b5656894b4d9a2db4c7124b600d
Chris Brinley <Chris.Brinley@morningstar.com>
2013-07-30 18:48:08 -0500

    Adding gluster peer lwr

---
+++ glusterfs/resources/peer.rb
@@ -0,0 +1,8 @@
+actions :create, :delete
+default_action :create
+
+attribute :name, 
+    :kind_of => String,
+    :regex => /^(?![0-9]+$)(?!-)[a-zA-Z0-9-]{,63}(?<!-)$/,
+    :name_attribute => true,
+attr_accessor :exists
